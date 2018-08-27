295cca9090fa8b7420ab069207d972ade92dbac4
Ievgen Kostiukevych <ievgenkostiukevych@ievgens-macbook-pro.home>
2018-08-27 21:54:17 +0200

    Code cleanup

--- eAPI_Port_Mirroring_v1.py
+++ eAPI_Port_Mirroring_v1.py
@@ -1,7 +1,7 @@
 # This tool is designed to utilize Arista eAPI in order to make json requests
 # and parse responses
 # Main functionality includes listing active port mirroring sessions, stopping
-# them and creating new 
+# them and creating new
 
 # Additional functionality is automated switching of the range of source ports
 
@@ -19,9 +19,7 @@ import pyeapi   # Arista eAPI module
 import tkinter.messagebox   # Pop up windows module
 
 
-#**************************FUNCTIONAL PART*************************************
-
-
+# **************************FUNCTIONAL PART*************************************
 
 
 class connect_to_switch(object):
@@ -37,8 +35,6 @@ class connect_to_switch(object):
         # connection initiated as switch object parameter
         self.connected_switch = pyeapi.connect(
             transport='https', host=self.ip_addr, username=self.username, password=self.password, port=None)
-
-       
 
         # As there is no usable way to parse connection errors on initiation state,
         # connection status is checked by executing simple "show hostname" command.
@@ -74,7 +70,8 @@ class monitor_sessions(object):
         self.used_source_ports = []
         self.used_destination_ports = []
         main_app.output_text.delete('1.0', tk.END)
-        main_app.output_text.insert(tk.END, 'Current port mirroring configuration:')
+        main_app.output_text.insert(
+            tk.END, 'Current port mirroring configuration:')
 
         self.active_sessions = switch.connected_switch.execute(
             ['show monitor session'])
@@ -117,7 +114,6 @@ class monitor_sessions(object):
 
 # "empty" monitor sessions object is created at the beggining
 current_sessions = monitor_sessions()
-
 
 
 class new_monitor_session(object):
@@ -199,13 +195,13 @@ class new_automated_session(object):
 
     def initiate(self):
         self.new_session_name = main_app.user_session_name_automate.get()
-                # Stops session for current ports
+        # Stops session for current ports
         try:
             switch.connected_switch.execute(
                 ['configure terminal', 'no monitor session {}'.format(self.new_session_name)])
         except:
-           pass
-           
+            pass
+
         # Class creates a list of source ports to go through from user input fields
         self.index = 0
         self.list_of_ports = main_app.user_list_of_ports.get()
@@ -215,7 +211,6 @@ class new_automated_session(object):
             self.port_range.append(port)
 
         self.current_port = self.port_range[self.index]
-
 
     def next_port(self):
         # goes through the ports list, kills currents session, initiates session creation for next port
@@ -264,7 +259,7 @@ class new_automated_session(object):
 
 def monitor_sessions_list():
     # Proxy function for the GUI button "refresh session list"
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         current_sessions.refresh_list()
     else:
         tkinter.messagebox.showerror('Error', 'Connect to the switch first!')
@@ -272,7 +267,7 @@ def monitor_sessions_list():
 
 def tx_session_creation():
     # Proxy function for TX only new session GUI button
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         new_session = new_monitor_session()
         new_session.tx_session_source()
         new_session.destination()
@@ -283,7 +278,7 @@ def tx_session_creation():
 
 def rx_session_creation():
     # Proxy function for TX only new session GUI button
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         new_session = new_monitor_session()
         new_session.rx_session_source()
         new_session.destination()
@@ -294,7 +289,7 @@ def rx_session_creation():
 
 def duplex_session_creation():
     # Proxy function for TX only new session GUI button
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         new_session = new_monitor_session()
         new_session.duplex_session_source()
         new_session.destination()
@@ -306,7 +301,7 @@ def duplex_session_creation():
 def kill_monitor_session():
     # Function to stop any active session
     # Session name is received from the text field
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
 
         session_to_kill = main_app.remove_session_name.get()
         try:
@@ -330,7 +325,7 @@ automated_session = new_automated_session()
 
 def start_duplex_automation():
     # Proxy function for automated session GUI button
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         automated_session.initiate()
         automated_session.duplex_session_source()
         automated_session.destination()
@@ -341,17 +336,18 @@ def start_duplex_automation():
 
 def next_automated():
     # Proxy function for next port GUI button
-    if main_app.switch_is_connected == True:
+    if main_app.switch_is_connected:
         automated_session.next_port()
         automated_session.destination()
         monitor_sessions_list()
     else:
         tkinter.messagebox.showerror('Error', 'Connect to the switch first!')
 
-#****************************GUI PART****************************************
+# ****************************GUI PART****************************************
+
 
 class MainApplication(tk.Frame):
-    
+
     def __init__(self, master):
         self.master = master
         tk.Frame.__init__(self, self.master)
@@ -367,27 +363,32 @@ class MainApplication(tk.Frame):
     def configure_gui(self):
         self.master.title('Arista port mirroring tool')
         self.master.resizable(False, False)
+
     def login_section(self):
         # Section title
-        self.login_first_label = tk.Label(self.master, text='Login First', font='Arial 12 bold')
+        self.login_first_label = tk.Label(
+            self.master, text='Login First', font='Arial 12 bold')
         self.login_first_label.grid(row=1, columnspan=3)
 
         # IP address label and input text field
-        self.ip_addr_label = tk.Label(self.master, text='Device IP address', font='Arial 10')
+        self.ip_addr_label = tk.Label(
+            self.master, text='Device IP address', font='Arial 10')
         self.ip_addr_label.grid(row=3, column=0, sticky=tk.E)
 
         self.ip_addr_entry = tk.Entry(self.master, font='Arial 10')
         self.ip_addr_entry.grid(row=3, column=1)
 
         # Username label and input text field
-        self.username_label = tk.Label(self.master, text='Username', font='Arial 10')
+        self.username_label = tk.Label(
+            self.master, text='Username', font='Arial 10')
         self.username_label.grid(row=4, column=0, sticky=tk.E)
 
         self.username_entry = tk.Entry(self.master, font='Arial 10')
         self.username_entry.grid(row=4, column=1)
 
         # Password label and input text field, password is hidden when entering
-        self.password_label = tk.Label(self.master, text='Password', font='Arial 10')
+        self.password_label = tk.Label(
+            self.master, text='Password', font='Arial 10')
         self.password_label.grid(row=5, column=0, sticky=tk.E)
 
         self.password_entry = tk.Entry(self.master, show='*', font='Arial 10')
@@ -395,25 +396,25 @@ class MainApplication(tk.Frame):
 
         # Login button. Calls for switch login function
         self.login_button = tk.Button(self.master, text='Login', width=30,
-                              command=switch_login, font='Arial 10')
+                                      command=switch_login, font='Arial 10')
         self.login_button.grid(row=6, columnspan=3)
 
         # Dummy field. Replaced by login status when connection is active
-        self.status_label = tk.Label(self.master, text='Connection Status', font='Arial 10')
+        self.status_label = tk.Label(
+            self.master, text='Connection Status', font='Arial 10')
         self.status_label.grid(row=7, columnspan=3)
-
 
     def connected(self):
         # Basic switch info is displayed when connected
-        self.status_bar = tk.Label(self.master, bg='green', font='Arial 10', text='Connected to: {} at {}'.format(switch.switch_hostname['result'][0]['hostname'], switch.ip_addr), bd=1, relief=tk.SUNKEN, anchor=tk.CENTER)
+        self.status_bar = tk.Label(self.master, bg='green', font='Arial 10', text='Connected to: {} at {}'.format(
+            switch.switch_hostname['result'][0]['hostname'], switch.ip_addr), bd=1, relief=tk.SUNKEN, anchor=tk.CENTER)
         self.switch_is_connected = True
-        
 
     def not_connected(self):
         # if not connected - error displayed
-        self.status_bar = tk.Label(self.master, bg='red', font='Arial 10', text='Connection error, check parameters!', bd=1, relief=tk.SUNKEN, anchor=tk.CENTER)
+        self.status_bar = tk.Label(self.master, bg='red', font='Arial 10',
+                                   text='Connection error, check parameters!', bd=1, relief=tk.SUNKEN, anchor=tk.CENTER)
         self.switch_is_connected = False
-        
 
     def display_label(self):
         # checks if the label is visible, destroys label when info is refreshed
@@ -422,73 +423,84 @@ class MainApplication(tk.Frame):
         except:
             pass
         finally:
-            self.status_bar.grid(row=7, columnspan=5, sticky=tk.N+tk.S+tk.E+tk.W)
-         
+            self.status_bar.grid(row=7, columnspan=5,
+                                 sticky=tk.N+tk.S+tk.E+tk.W)
 
     def new_section_section(self):
         # Section title
-        self.new_session_label = tk.Label(self.master, text='Create or modify session', font='Arial 12 bold')
+        self.new_session_label = tk.Label(
+            self.master, text='Create or modify session', font='Arial 12 bold')
         self.new_session_label.grid(row=10, columnspan=3)
 
         # Session name label and text field
-        self.session_name_label = tk.Label(self.master, text='Session name', font='Arial 10')
+        self.session_name_label = tk.Label(
+            self.master, text='Session name', font='Arial 10')
         self.session_name_label.grid(row=11, column=0, sticky=tk.E)
 
         self.user_session_name = tk.Entry(self.master, font='Arial 10')
         self.user_session_name.grid(row=11, column=1)
 
         # Source port label and text field
-        self.source_port_label = tk.Label(self.master, text='Source port', font='Arial 10')
+        self.source_port_label = tk.Label(
+            self.master, text='Source port', font='Arial 10')
         self.source_port_label.grid(row=12, column=0, sticky=tk.E)
 
         self.user_source_port = tk.Entry(self.master, font='Arial 10')
         self.user_source_port.grid(row=12, column=1)
 
         # Destination port label and text field
-        self.destination_port_label = tk.Label(self.master, text='Destination port', font='Arial 10')
+        self.destination_port_label = tk.Label(
+            self.master, text='Destination port', font='Arial 10')
         self.destination_port_label.grid(row=13, column=0, sticky=tk.E)
-
 
         self.user_destination_port = tk.Entry(self.master, font='Arial 10')
         self.user_destination_port.grid(row=13, column=1)
 
         # TX only button. Calls for TX only new session function
-        self.tx_only_button = tk.Button(self.master, text='Source port: TX only', width=30, command=tx_session_creation, font='Arial 10')
+        self.tx_only_button = tk.Button(
+            self.master, text='Source port: TX only', width=30, command=tx_session_creation, font='Arial 10')
         self.tx_only_button.grid(row=14, columnspan=3)
 
         # RX only button. Calls for RX only new session function
-        self.rx_only_button = tk.Button(self.master, text='Source port: RX only', width=30, command=rx_session_creation, font='Arial 10')
+        self.rx_only_button = tk.Button(
+            self.master, text='Source port: RX only', width=30, command=rx_session_creation, font='Arial 10')
         self.rx_only_button.grid(row=15, columnspan=3)
 
         # Duplex button. Calls for duplex new session function
-        self.duplex_button = tk.Button(self.master, text='Source port: Duplex', width=30, command=duplex_session_creation, font='Arial 10')
+        self.duplex_button = tk.Button(
+            self.master, text='Source port: Duplex', width=30, command=duplex_session_creation, font='Arial 10')
         self.duplex_button.grid(row=16, columnspan=3)
 
     def remove_session_section(self):
 
         # Section title
-        self.remove_session_label = tk.Label(self.master, text='Remove session', font='Arial 12 bold')
+        self.remove_session_label = tk.Label(
+            self.master, text='Remove session', font='Arial 12 bold')
         self.remove_session_label.grid(row=17, columnspan=3)
 
         # Session to remove name text field and title
-        self.session_remove_name_label = tk.Label(self.master, text='Session name', font='Arial 10')
+        self.session_remove_name_label = tk.Label(
+            self.master, text='Session name', font='Arial 10')
         self.session_remove_name_label.grid(row=19, column=0, sticky=tk.E)
 
         self.remove_session_name = tk.Entry(self.master, font='Arial 10')
         self.remove_session_name.grid(row=19, column=1)
 
         # Session remove button, calls session remove function
-        self.remove_session_button = tk.Button(self.master, text='Remove session', width=30, command=kill_monitor_session, font='Arial 10')
+        self.remove_session_button = tk.Button(
+            self.master, text='Remove session', width=30, command=kill_monitor_session, font='Arial 10')
         self.remove_session_button.grid(row=20, columnspan=3)
 
     def session_list_section(self):
-        
+
         # Section title
-        self.list_label = tk.Label(self.master, text='Active sessions', font='Arial 12 bold')
+        self.list_label = tk.Label(
+            self.master, text='Active sessions', font='Arial 12 bold')
         self.list_label.grid(row=21, columnspan=3)
 
         # Session refresh button. Calls session list refresh function
-        self.sessions_list_button = tk.Button(self.master, text='Refresh sessions list', width=30, command=monitor_sessions_list, font='Arial 10')
+        self.sessions_list_button = tk.Button(
+            self.master, text='Refresh sessions list', width=30, command=monitor_sessions_list, font='Arial 10')
         self.sessions_list_button.grid(row=22, columnspan=3)
 
         # Main text output filed
@@ -510,7 +522,7 @@ How to do it? See:
 
 https://eos.arista.com/forum/how-do-i-enable-configure-commands-via-http-api/
 
-            
+
 Copyright 2018 Ievgen Kostiukevych
 https://github.com/ievgen-kostiukevych
 
@@ -530,47 +542,54 @@ either express or implied.
 See the License for the specific language
 governing permissions and limitations
 under the License.
-""")    
+""")
 
     def automated_switching(self):
 
         # Section title
-        self.list_label = tk.Label(self.master, text='Automated switching (Duplex only)', font='Arial 12 bold')
+        self.list_label = tk.Label(
+            self.master, text='Automated switching (Duplex only)', font='Arial 12 bold')
         self.list_label.grid(row=23, columnspan=3)
 
         # Session name label and text field
-        self.session_name_label_automate = tk.Label(self.master, text='Session name', font='Arial 10')
+        self.session_name_label_automate = tk.Label(
+            self.master, text='Session name', font='Arial 10')
         self.session_name_label_automate.grid(row=24, column=0, sticky=tk.E)
 
-        self.user_session_name_automate = tk.Entry(self.master, font='Arial 10')
+        self.user_session_name_automate = tk.Entry(
+            self.master, font='Arial 10')
         self.user_session_name_automate.grid(row=24, column=1)
 
         # Start source port label and text field
-        self.start_source_port_label = tk.Label(self.master, text='List of source ports', font='Arial 10')
+        self.start_source_port_label = tk.Label(
+            self.master, text='List of source ports', font='Arial 10')
         self.start_source_port_label.grid(row=25, column=0, sticky=tk.E)
 
         self.user_list_of_ports = tk.Entry(self.master, font='Arial 10')
         self.user_list_of_ports.grid(row=25, column=1)
 
         # Destination port label and text field
-        self.destination_port_label_automate = tk.Label(self.master, text='Destination port', font='Arial 10')
-        self.destination_port_label_automate.grid(row=27, column=0, sticky=tk.E)
+        self.destination_port_label_automate = tk.Label(
+            self.master, text='Destination port', font='Arial 10')
+        self.destination_port_label_automate.grid(
+            row=27, column=0, sticky=tk.E)
 
-        self.user_destination_port_automate = tk.Entry(self.master, font='Arial 10')
+        self.user_destination_port_automate = tk.Entry(
+            self.master, font='Arial 10')
         self.user_destination_port_automate.grid(row=27, column=1)
 
-
         # Start automation button, initiates port-by-port monitoring sessions
-        self.duplex_only_button = tk.Button(self.master, text='Start', width=30, command=start_duplex_automation, font='Arial 10')
+        self.duplex_only_button = tk.Button(
+            self.master, text='Start', width=30, command=start_duplex_automation, font='Arial 10')
         self.duplex_only_button.grid(row=29, columnspan=3)
 
-
         # Next port button
-        self.next_button = tk.Button(self.master, text='Next port', width=30, command=next_automated, font='Arial 10')
+        self.next_button = tk.Button(
+            self.master, text='Next port', width=30, command=next_automated, font='Arial 10')
         self.next_button.grid(row=31, padx=1, columnspan=3)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    main_app =  MainApplication(root)
+    main_app = MainApplication(root)
     root.mainloop()
